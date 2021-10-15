f1e8ef7255cc706e4f592da6eb3fa437c0a2504b
Simon Rodriguez <esimonrodrig@gmail.com>
2021-10-15 14:23:11 -0400

    conditionally export parse based on the environment

--- src/services/initParse.ts
+++ src/services/initParse.ts
@@ -1,18 +1,22 @@
-import Parse from 'parse/node';
+import Parse from 'parse';
+import NodeParse from 'parse/node';
 import dotenv from 'dotenv';
 
-const initParse = () => {
-    dotenv.config();
+// Read .env file and ad to environment
+dotenv.config();
 
-    const parseId = process.env.PARSE_APP_ID;
-    const jsId    = process.env.PARSE_JS_KEY;
+// Because Jest runs in NodeJS we need to conditionally use the Node Parse object or Browser
+// https://stackoverflow.com/a/34550964
+const parseObj = (typeof window === 'undefined') ? NodeParse : Parse;
 
-    if (parseId && jsId) {
-        Parse.initialize(parseId, jsId);
-        Parse.serverURL = 'https://parseapi.back4app.com/';
-    } else {
-        throw new Error('Parse environment variables not initialized!! Did you make a .env?');
-    }
-};
+const parseId = process.env.PARSE_APP_ID;
+const jsId    = process.env.PARSE_JS_KEY;
 
-export default initParse;
+if (parseId && jsId) {
+    parseObj.initialize(parseId, jsId);
+    parseObj.serverURL = 'https://parseapi.back4app.com/';
+} else {
+    throw new Error('Parse environment variables not initialized!! Did you make a .env?');
+}
+
+export default parseObj;
