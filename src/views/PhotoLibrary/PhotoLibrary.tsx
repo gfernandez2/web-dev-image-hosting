bc61f9acdc63523f5e308074a572fa61f147173f
Simon Rodriguez <esimonrodrig@gmail.com>
2021-10-14 20:18:59 -0400

    minor changes

--- src/views/PhotoLibrary/PhotoLibrary.tsx
+++ src/views/PhotoLibrary/PhotoLibrary.tsx
@@ -1,5 +1,8 @@
 import React, { FC } from 'react';
 
+// This is how to import scss styles into a component
+import './PhotoLibrary.scss';
+
 // Note: FC is "Functional Component", don't think it's needed but may be helpful
 const PhotoLibrary: FC = () => {
 
